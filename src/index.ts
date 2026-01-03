567e7c78c90b15f4a193f34163724e0835c97ee6
1m4nim <fancustard9@gmail.com>
2026-01-03 21:25:54 +0900

    2026/01/03 プリミティブ型同士の変換

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
-const message:string="Hello World";
+// const message:string="Hello World";
+// console.log(message)
 
-console.log(message)
+//2.3プリミティブ型
+// const width1 = 5;
+// const width2 = 8;
+// const height: number = 3;
+// const area = ((width1 + width2) * height) / 2;
+// console.log(area);
+
+import { createInterface } from "readline";
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+rl.question("文字列を入力してください：", (line: string) => {
+  console.log(`${line}が入力されました`);
+  rl.close();
+});
