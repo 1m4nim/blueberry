ed1f742aab647859c43b2a1befca48a5c48c0f3c
1m4nim <fancustard9@gmail.com>
2026-01-03 21:33:56 +0900

    2026/01/03 明示的な型変換

--- src/index.ts
+++ src/index.ts
@@ -8,13 +8,24 @@
 // const area = ((width1 + width2) * height) / 2;
 // console.log(area);
 
-import { createInterface } from "readline";
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// import { createInterface } from "readline";
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("文字列を入力してください：", (line: string) => {
+//   console.log(`${line}が入力されました`);
+//   rl.close();
+// });
 
-rl.question("文字列を入力してください：", (line: string) => {
-  console.log(`${line}が入力されました`);
+
+import { createInterface } from "node:readline";
+ const rl=createInterface({
+  input:process.stdin,
+  output:process.stdout
+ });
+rl.question("数値を入力してください：",(line)=>{
+  const num =Number(line);
+  console.log(num+1000);
   rl.close();
-});
+})
