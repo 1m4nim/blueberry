c9adccb11016aba68c06cb801ceaa6a797609047
1m4nim <fancustard9@gmail.com>
2026-01-04 22:04:03 +0900

    while文のループ

--- src/index.ts
+++ src/index.ts
@@ -52,22 +52,55 @@ console.log(`secretは${secret}です`);
 // console.log(`secretは${secret}です`);
 // // SECRET=Hello node dist/index.jsで変わる
 
-import { createInterface } from "node:readline";
-const rl = createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
-rl.question("コマンドを入力してください：", (name) => {
-  switch (name) {
-    case "greet":
-      console.log("こんにちは");
-      break;
-    case "cat":
-      console.log("あなたは猫派？");
-      break;
-    default:
-      console.log(`コマンド${name}を認識できませんでした`);
+// import { createInterface } from "node:readline";
+// const rl = createInterface({
+//   input: process.stdin,
+//   output: process.stdout,
+// });
+// rl.question("コマンドを入力してください：", (name) => {
+//   switch (name) {
+//     case "greet":
+//       console.log("こんにちは");
+//       break;
+//     case "cat":
+//       console.log("あなたは猫派？");
+//       break;
+//     default:
+//       console.log(`コマンド${name}を認識できませんでした`);
+//   }
+//   rl.close();
+// });
+
+// let sum = 0;
+// let i = 1;
+// while (i <= 100) {
+//   sum += i;
+//   i++;
+// }
+// console.log(sum); //5050
+
+// let sum = 0;
+// let i = 1;
+// while (true) {
+//   if (i > 100) {
+//     break;
+//   }
+//   sum += i;
+//   i++;
+// }//無限ループを止める
+
+let i = 1;
+while (i <= 100) {
+  i++;
+  if (i % 2 === 1) {
+    //偶数のときだけ
+    continue; //whileに戻る
   }
+<<<<<<< HEAD
   rl.close();
 });
 >>>>>>> 31267d5 (2026/01/04 switch文)
+=======
+  console.log(i);
+}
+>>>>>>> 52bfa30 (while文のループ)
