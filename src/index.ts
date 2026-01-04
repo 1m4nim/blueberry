b2c28952230480bc92d0d0f6be8fe4363553cbfc
1m4nim <fancustard9@gmail.com>
2026-01-04 21:53:39 +0900

    2026/01/04 switch文

--- src/index.ts
+++ src/index.ts
@@ -42,7 +42,32 @@ console.log(message)
 //   rl.close();
 // });
 
+<<<<<<< HEAD
 const secret = process.env.SECRET ?? "default";
 console.log(`secretは${secret}です`);
 // SECRET=Hello node dist/index.jsで変わる
 
+=======
+// const secret = process.env.SECRET ?? "default";
+// console.log(`secretは${secret}です`);
+// // SECRET=Hello node dist/index.jsで変わる
+
+import { createInterface } from "node:readline";
+const rl = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+rl.question("コマンドを入力してください：", (name) => {
+  switch (name) {
+    case "greet":
+      console.log("こんにちは");
+      break;
+    case "cat":
+      console.log("あなたは猫派？");
+      break;
+    default:
+      console.log(`コマンド${name}を認識できませんでした`);
+  }
+  rl.close();
+});
+>>>>>>> 31267d5 (2026/01/04 switch文)
